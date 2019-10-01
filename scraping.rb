7a8e6100d8f634c2bba401eef75f61dca2ea3e58
otuhs_d <yekcim.114@gmail.com>
2019-10-01 18:58:55 +0900

    refactoring

--- scraping.rb
+++ scraping.rb
@@ -7,7 +7,7 @@ driver = Selenium::WebDriver.for :remote, desired_capabilities: :chrome
 
 def driver.find_and_wait_element(how, what)
   Selenium::WebDriver::Wait.new(timeout: 10).until{ find_element(how, what) }
-  sleep 2
+  sleep 2 # コンテンツがヌルっと出てくるのでちょっと待たないとクリック等ができない
   find_element(how, what)
 end
 
@@ -20,19 +20,15 @@ driver.find_element(:id, 'loginId').send_keys(ENV['LOGINID'])
 driver.find_element(:id, 'passWord').send_keys(ENV['PASSWORD'])
 driver.find_element(:xpath, '//*[@id="login"]/a').click
 
-# `電力使用量・ご請求` へ
-to_amount_claim = driver.find_and_wait_element(:xpath, '//*[@id="auto"]/section/div/div[2]/nav/ul/li[2]/p/a')
-to_amount_claim.click
-
-# `電力使用量を見る` へ
-to_amount_confirmation = driver.find_and_wait_element(:id, 'submitReferenceCrrspndDetailsPrtl')
-to_amount_confirmation.click
+# 電力使用量ページへ
+driver.find_and_wait_element(:xpath, '//*[@id="auto"]/section/div/div[2]/nav/ul/li[2]/p/a').click
+driver.find_and_wait_element(:id, 'submitReferenceCrrspndDetailsPrtl').click
 
 # 日付を指定
-target_date = Date.today - 1
+yesterday = Date.today - 1
 date_field = driver.find_and_wait_element(:id, 'EntryInputForm_entryModel_svcStrtDt')
 date_field.clear
-date_field.send_keys(target_date.strftime("%Y/%m/%d"))
+date_field.send_keys(yesterday.strftime("%Y/%m/%d"))
 
 # 表示形式を指定
 time_indication_type = Selenium::WebDriver::Support::Select.new(driver.find_and_wait_element(:id, 'timeIndicationType'))
@@ -45,7 +41,7 @@ data = driver.page_source.scan(/source\.data\.push\(\"(\d\.\d{2})\"\)/).flatten.
 session = GoogleDrive::Session.from_config('config.json')
 ws = session.spreadsheet_by_key(ENV['SPREADSHEET_KEY']).worksheets[0]
 48.times do |n|
-  datetime = DateTime.parse(target_date.to_s) + Rational(0.5 * n, 24)
+  datetime = DateTime.parse(target_date.to_s) + Rational(0.5 * n, 24) # 30 分刻み
   new_row = ws.num_rows + 1
   ws[new_row, 1] = datetime.strftime("%Y/%m/%d %H:%M:%S")
   ws[new_row, 2] = data[n]
